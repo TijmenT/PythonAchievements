1bd200fc50fff580bb38e571ade8696d753cce1d
Tijmen <30957@ma-web.nl>
2020-09-02 12:13:51 +0200

    gedichttijmen

---
+++ gedicht.py
@@ -0,0 +1,4 @@
+vers1 = "Ik ben Tijmen, ik ben 16 jaar oud en ik woon in Opperdoes. Mijn reistijd van Opperdoes naar het Media college is 1,5 uur. Ik hoop hier veel te leren en ik heb er veel zin in!"
+vers2 = "Ik ben sinds gisteren heel geintresseerd geraakt in Python, het is makkelijk te gebruiken en je kan er van alles mee doen!"
+
+print(vers1,vers2)
