89516ae8f063279ed9b6985998c4ece44d924fa7
Tijmen <30957@ma-web.nl>
2020-09-16 15:28:01 +0200

    Appels

---
+++ PYTB1L2HowYouLikeThemApples.py
@@ -0,0 +1,23 @@
+import math
+
+
+trees               = 333
+shadedTrees         = math.ceil(220)
+sunnyTrees          = 110
+
+shadeOutputModifier = 80
+
+sunnyTreeOutput     = 146
+shadedTreeOutput    = 117
+
+sunnyOutput         = 16060 
+shadedOutput        = 25740
+totalOutput         = 41800
+
+owners              = 3
+
+eatCount            = 1
+sellableOutput      = 41.799
+cut                 = 13.933
+
+print(cut)
